dfaadc9aef85445855229eaf2557e23def797ded
irfan44 <irfannmuhajir@gmail.com>
2023-02-02 22:38:28 +0700

    feat: add get chrome extension

---
+++ src/components/extension/GetExtension.jsx
@@ -0,0 +1,62 @@
+import { Typography } from 'antd';
+import { Modal } from 'antd';
+import { useState } from 'react';
+import { FaRegCopy } from 'react-icons/fa';
+import { IoExtensionPuzzle } from 'react-icons/io5';
+
+const { Text } = Typography;
+
+const GetExtension = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <a className="flex items-center space-x-2" onClick={showModal}>
+        <IoExtensionPuzzle />
+        <span>Get Extension</span>
+      </a>
+      <Modal
+        title="Get Emoview Extension"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="Close"
+        cancelButtonProps={{ hidden: true }}
+      >
+        <p className="mb-4">
+          Emoview Extension is used to capture emotion of the participants of
+          your meetings. Participant's emotion will show up in Emoview's chart
+          at Meeting Details page *
+        </p>
+        <div className="flex flex-col mb-4">
+          <span>Get extension **</span>
+          <Text copyable={{ icon: <FaRegCopy className="text-black" /> }}>
+            <a>https://google.com</a>
+          </Text>
+        </div>
+        <p className="mb-2 text-black/[.60] text-xs">
+          * Participant's emotion will not show up if they didn't install this
+          extension
+        </p>
+        <p className="text-black/[.60] text-xs">
+          ** This extension is only for use in Google Chrome, Microsoft Edge, or
+          other Chromium based browser
+        </p>
+      </Modal>
+    </>
+  );
+};
+
+export default GetExtension;
